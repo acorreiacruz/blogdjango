1a0a02c70b4669724e62ca07cb334764bafaf21d
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-02-01 17:03:13 -0300

    Criando models
    Criando o model Post que será a base do blog.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from email.quoprimime import body_check
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100,null=False)
+    slug = models.SlugField(null=False)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    body = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
